ea32ba8f19635d1c4beb7575f9d0c31a69d955b2
Thomas Burns <tcburns24@gmail.com>
2018-08-26 22:10:20 -0400

    Adds guessWord and guessLetter, start-game modal needs revision

--- js/main.js
+++ js/main.js
@@ -39,9 +39,13 @@ function newGame(param) {
 	}
 }
 
+function showModal(modalName) {
+	document.getElementsByClassName(modalName)[0].style.display = 'inline';
+	document.getElementsByClassName('game')[0].style.opacity = '0.4';
+}
 
-function checkLetter() {
-	console.log('checkLetter works');
+
+function correctLetter() {
 	var letter = guessLetterBox.value;
 	for (var i=0; i<gameWord.length; i++) {
 		if (gameWord[i] == letter) {
@@ -59,13 +63,92 @@ function checkLetter() {
 	guessLetterBox.value = '';
 }
 
+
+function incorrectLetter() {
+	for (var i=0; i<bodyParts.length; i++) {
+		if (bodyParts[i].style.display == 'none') {
+			bodyParts[i].style.display = 'inline';
+			break;
+		}
+	}
+	for (var i=0; i<letterBoxes.length; i++) {
+		letterBoxes[i].style.borderColor = '#bb2124';
+	}
+	setTimeout(function() {
+		for (var i=0; i<letterBoxes.length; i++) {
+			if (letterBoxes[i].style.borderColor != '#95C0F7') {
+				letterBoxes[i].style.borderColor = '#95C0F7';
+			}
+		}
+	}, 500)
+	guessLetterBox.value = '';
+}
+
+
+function checkLetter() {
+	for (var i=0; i<document.getElementsByClassName('letterdiv').length; i++) {
+		if (document.getElementsByClassName('letterdiv')[i].textContent == guessLetterBox.value.toUpperCase()) {
+			document.getElementsByClassName('letterdiv')[i].style.color = 'grey';
+			document.getElementsByClassName('letterdiv')[i].style.textDecoration = 'line-through';
+		}
+	}
+	if (gameWord.includes(guessLetterBox.value)) {
+		correctLetter();
+	} else {
+		incorrectLetter();
+	}
+}
+
+
+function correctWord() {
+	for (var i=0; i<gameWord.length; i++) {
+		letterBoxes[i].style.color = '#092C5C';
+		letterBoxes[i].style.borderColor = '#22bb33';
+	}
+	// endGame() 
+}
+
+
+function incorrectWord() {
+	for (var i=0; i<bodyParts.length; i++) {
+		if (bodyParts[i].style.display == 'none') {
+			bodyParts[i].style.display = 'inline';
+			break;
+		}
+	}
+	for (var i=0; i<letterBoxes.length; i++) {
+		letterBoxes[i].style.borderColor = '#bb2124';
+	}
+	setTimeout(function() {
+		for (var i=0; i<letterBoxes.length; i++) {
+			if (letterBoxes[i].style.borderColor != '#95C0F7') {
+				letterBoxes[i].style.borderColor = '#95C0F7';
+			}
+		}
+	}, 500)
+	guessWordBox.value = '';
+}
+
+function checkWord() {
+	if (guessWordBox.value.toLowerCase() == gameWord) {
+		return correctWord();
+	} else {
+		return incorrectWord();
+	}
+}
+
+
 guessLetterBox.addEventListener('keyup', function(event) {
 	if (event.keyCode === 13) {
 		checkLetter();	
 	}
 })
 
-
+guessWordBox.addEventListener('keyup', function(event) {
+	if (event.keyCode === 13) {
+		checkWord();
+	}
+})
 
 
 
